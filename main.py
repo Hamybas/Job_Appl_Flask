3d7e80a2b258caa293b455a61dd45ddcbfc650ff
Mobas <gadiner1@gmail.com>
2023-12-23 11:52:52 +0300

    Creating a data variables and DB from SQLAlchemy

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+app.run(debug=False, port=5001)
 
 
