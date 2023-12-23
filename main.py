59b6886c5e4a4281c5be07f5bab0ba56f57e70d7
Mobas <gadiner1@gmail.com>
2023-12-23 12:10:38 +0300

    Creating a data input form and store it

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+from datetime import datetime
+import sqlalchemy
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("PASSWORD")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    status = db.Column(db.String(80))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,11 +27,22 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
-        occupation = request.form["occupation"]
+        date_object = datetime.strptime(date, "%Y-%m-%d")
+        status = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name, email=email,
+                    date=date_object, status=status)
+        db.session.add(form)
+        db.session.commit()
+
 
     return render_template("index.html")
 
 
-app.run(debug=False, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
+
 
 
